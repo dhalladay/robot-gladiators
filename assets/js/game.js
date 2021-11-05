5c57bde48a48303d112bc4715efb627c338b707d
dhalladay <dave.halladay@outlook.com>
2021-11-04 18:54:19 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,8 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(Math)
+
 //fight function
 var fight = function(enemyName) {
     //repeat and execute as long as the enemy-robot is alive
@@ -23,14 +25,16 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip the fight. Goodbye!");
                 //subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         } 
 
         //remove enemy's health by subtracting amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -49,7 +53,9 @@ var fight = function(enemyName) {
         }
 
         // remove player's health by subtracting amount set in enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -82,7 +88,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
 
             //reset enemyHealth
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             //use debugger to pause script
             // debugger;
@@ -177,6 +183,11 @@ var shop = function () {
       shop();
       break;
   }
+};
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
 };
 
 //start the game when the page loads
